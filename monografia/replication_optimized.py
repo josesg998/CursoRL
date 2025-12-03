b5cff17c20400f97abf383b34c3644a84a614000
josesg998 <josesg998@gmail.com>
2025-12-03 13:23:02 -0300

    agrego evolucion de susceptibilidad

--- monografia/replication_optimized.py
+++ monografia/replication_optimized.py
@@ -10,8 +10,9 @@ def juego_completo_numba(W_NOR_POS, W_NOR_NEG, W_EMP_POS, W_EMP_NEG,seed,
     aspiracion_alfa = np.random.uniform(PHI/2, PHI, N)
     suscept = np.random.uniform(0, 0.5, N)
     dinero_donado_delta = np.zeros(N)
+    suscept_history = np.zeros(rondas)
     
-    for _ in range(rondas):
+    for r in range(rondas):
     
         indices = np.arange(N)
         np.random.shuffle(indices)
@@ -105,7 +106,10 @@ def juego_completo_numba(W_NOR_POS, W_NOR_NEG, W_EMP_POS, W_EMP_NEG,seed,
                 aspiracion_alfa[idx_receptor] += (PHI - aspiracion_alfa[idx_receptor]) * suscept[idx_receptor] * estimulo_emp
             else:
                 aspiracion_alfa[idx_receptor] += aspiracion_alfa[idx_receptor] * suscept[idx_receptor] * estimulo_emp
-    return suscept, dinero_donado_delta
+        
+        suscept_history[r] = np.mean(suscept)
+
+    return suscept, dinero_donado_delta, suscept_history
 
 
 RANGOS = [0,1/3,2/3,1]
@@ -120,9 +124,10 @@ for W_NOR_POS in RANGOS:
             for W_EMP_NEG in RANGOS:
                 dineros_donados = []
                 susceptibilidades = []
+                suscept_histories = []
                 for seed in seeds:
-                    print(f"Corriendo simulación con W_NOR_POS={W_NOR_POS}, W_NOR_NEG={W_NOR_NEG}, W_EMP_POS={W_EMP_POS}, W_EMP_NEG={W_EMP_NEG} semilla {seed}")
-                    suscept, dinero_donado_delta = juego_completo_numba(W_NOR_POS=W_NOR_POS, 
+                    print(f"Corriendo simulación con W_NOR_POS={W_NOR_POS:.2f}, W_NOR_NEG={W_NOR_NEG:.2f}, W_EMP_POS={W_EMP_POS:.2f}, W_EMP_NEG={W_EMP_NEG:.2f} semilla {seed}")
+                    suscept, dinero_donado_delta, suscept_history = juego_completo_numba(W_NOR_POS=W_NOR_POS, 
                                                                         W_NOR_NEG=W_NOR_NEG, 
                                                                         W_EMP_POS=W_EMP_POS, 
                                                                         W_EMP_NEG=W_EMP_NEG,
@@ -130,20 +135,27 @@ for W_NOR_POS in RANGOS:
                     
                     dineros_donados.append(np.sort(dinero_donado_delta))
                     susceptibilidades.append(np.sort(suscept))
+                    suscept_histories.append(suscept_history)
                 
                 dinero_donado_theta_final = np.mean(dineros_donados,axis=0)
                 suscept_final = np.mean(susceptibilidades,axis=0)
-                    
+                suscept_history_final = np.mean(suscept_histories, axis=0)
+                suscept_history_final = [float(x) for x in suscept_history_final]
+                
                 resultados.append(
                     {
                         "W_NOR_POS": W_NOR_POS,
                         "W_NOR_NEG": W_NOR_NEG,
                         "W_EMP_POS": W_EMP_POS,
                         "W_EMP_NEG": W_EMP_NEG,
-                        'susceptibilidaes': list(suscept_final),
+                        'susceptibilidades': list(suscept_final),
                         'donaciones_delta': list(dinero_donado_theta_final),
+                        'suscept_history': list(suscept_history_final),
                     }
                 )
-print("Guardando resultados en 'resultados_simulacion_optimizada_10_seeds.json'")
-with open("CursoRL/monografía/resultados_simulacion_optimizada_10_seeds.json", "w") as f:
+                
+file_name = "simulacion_10_seeds_susceptibility.json"
+                
+print(f"Guardando resultados en '{file_name}'")
+with open(f"monografia/{file_name}", "w") as f:
     json.dump(resultados, f)
